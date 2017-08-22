8e9a1cd5a89532000fd8524d0521e69503af12d8
Danilo <dnp1@users.noreply.github.com>
2017-08-22 20:59:40 -0300

    Login improvements

--- lib/components/login_component.dart
+++ lib/components/login_component.dart
@@ -1,10 +1,17 @@
 import 'package:angular2/angular2.dart';
+import 'package:danilo_info/model/login.dart';
 
 @Component(
   selector: 'dnp1-login',
   templateUrl: "login_component.html",
   styleUrls: const ['login_component.css', "styles/dnp1_page.css"],
+  directives: const [COMMON_DIRECTIVES]
 )
 class LoginComponent {
+  Login login = new Login();
 
+  onSubmit() {
+    print('teste');
+    login.email += 'e';
+  }
 }
---
+++ lib/model/login.dart
@@ -0,0 +1,4 @@
+class Login {
+  String email;
+  String password;
+}
