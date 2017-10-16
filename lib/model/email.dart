2691afdc601b6fe1f45f61ccda3712f6a1859d1a
Danilo <dnp1@users.noreply.github.com>
2017-10-16 20:35:09 -0200

    getting start with EmailService

---
+++ lib/model/email.dart
@@ -0,0 +1,10 @@
+
+class Email {
+  String id;
+  bool primary;
+  bool checked;
+  String address;
+  DateTime registrationDate;
+
+  Email(this.id, this.primary, this.address, this.registrationDate);
+}
---
+++ lib/services/email_service.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:angular/di.dart';
+import 'package:danilo_info/model/email.dart';
+
+@Injectable()
+class EmailService {
+  Future<List<Email>> ofUser(String userId) async {
+    if (userId=='1') {
+      return [new Email(
+        '1',
+        true,
+        'user@danilo.info',
+        new DateTime(2017, 1,1),
+      )];
+    }
+    return const [];
+  }
+}
