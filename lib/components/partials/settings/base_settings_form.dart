11b1dcba7c58c34c8ab21b9c3c3ae935f19c2bb0
Danilo Pereira <dnp1@users.noreply.github.com>
2017-10-31 14:54:38 -0200

    Format file

--- lib/components/partials/settings/base_settings_form.dart
+++ lib/components/partials/settings/base_settings_form.dart
@@ -6,13 +6,17 @@ import 'package:danilo_info/components/base/form_helper.dart';
 
 abstract class BaseSettingsForm {
   bool sending = false;
+
   Map<String, bool> controlStateClasses(NgControl control) =>
       FormHelper.controlStateClasses(control);
   @Input()
   String userId;
   final StreamController<Null> _dispose = new StreamController.broadcast();
+
   @Output()
   Stream<Null> get onDispose => _dispose.stream;
+
   void dispose() => _dispose.add(null);
+
   void save();
 }
--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,18 @@
 // Copyright (c) 2017. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 import 'package:angular/angular.dart';
-// import 'package:http/http.dart';
-
 
 import 'package:angular_router/angular_router.dart';
 import 'package:danilo_info/app_component.dart';
+import 'package:danilo_info/util/auth_client.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 
 void main() {
-  bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+  bootstrap(AppComponent,
+      [
+        ROUTER_PROVIDERS,
+        provide(Client, useFactory: () => new BrowserClient(), deps: []),
+        AuthClient,
+      ]);
 }
