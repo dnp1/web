de9a70abc7a7f5573a42b58ab4ee8a0c91196d41
Danilo Pereira <dnp1@users.noreply.github.com>
2017-11-21 12:44:05 -0200

    partially fix file upload

--- lib/services/user_avatar_service.dart
+++ lib/services/user_avatar_service.dart
@@ -4,17 +4,25 @@ import 'dart:html';
 import 'package:angular/di.dart';
 import 'package:danilo_info/services/base_http_service.dart';
 import 'package:danilo_info/util/auth_client.dart';
+import 'package:http/http.dart';
+import 'package:http_parser/http_parser.dart';
 
 @Injectable()
 class UserAvatarService extends BaseHttpService {
   UserAvatarService(AuthClient http) : super(http);
 
-  Future<Null> create(File file, String userId, String password) async {
+  Future<Null> create(String value, String filename, String contentType, String userId, String password) async {
     try {
-      var formData = new FormData();
-      formData.appendBlob('file', file, file.name);
-      final resp = await http.post(urlPrefix + '/file', body: formData);
-      var data = extractData(resp);
+      var uri = Uri.parse(urlPrefix + "/file");
+      var request = new MultipartRequest("POST", uri);
+      var _type = new MediaType.parse(contentType);
+      request.files.add(new MultipartFile.fromString("file", value, filename: filename, contentType: _type));
+
+      var send = await http.send(request);
+      window.alert("dshada");
+//      final resp = await http.post(urlPrefix + '/file', body: formData);
+      final resp = await send.stream.first;
+      var data = JSON.decode(UTF8.decode(resp));
       await http.put(
         '/user/$userId/avatar',
         body: JSON.encode({
--- lib/util/auth_client.dart
+++ lib/util/auth_client.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:angular/di.dart';
 import 'package:http/browser_client.dart' as base;
 import 'package:http/http.dart';
-import 'package:cookie/cookie.dart' as cookie;
 
 @Injectable()
 class AuthClient extends base.BrowserClient {
@@ -19,7 +18,7 @@ class AuthClient extends base.BrowserClient {
   /// Sends an HTTP request and asynchronously returns the response.
   Future<StreamedResponse> send(BaseRequest request) async {
     await load();
-    var resp = await super.send(request);
+    var resp = await _inner.send(request);
     return resp;
   }
 
@@ -28,44 +27,38 @@ class AuthClient extends base.BrowserClient {
   bool _loading = false;
   final StreamController loaded = new StreamController.broadcast();
 
-
-  bool _hasToken() {
-    return cookie.get(tokenName) != null;
-  }
-
   Future<Null> load() async {
     if (_loading) {
       await loaded.stream.first;
     }
     if (_loaded == false) {
       _loading = true;
-
-      var resp = await _inner.get(urlPrefix + "/session");
-      if (resp.statusCode == HttpStatus.OK) {
+      try {
+        await _inner.get(urlPrefix + "/session");
         _loaded = true;
         _loading = false;
         loaded.add(true);
         return;
-      }
-
-      try {
-        final resp = await _inner.post(urlPrefix + "/session");
-        if (resp.statusCode == HttpStatus.OK) {
-          _loaded = true;
-        } else {
-          _loaded = false;
+      } on ClientException catch (_) {
+        try {
+          final resp = await _inner.post(urlPrefix + "/session");
+          if (resp.statusCode == HttpStatus.OK) {
+            _loaded = true;
+          } else {
+            _loaded = false;
+          }
+        } catch (e) {
+          window.alert("shit is happening"); // TODO remove it
+        } finally {
+          _loading = false;
+          loaded.add(true);
         }
-      } catch (e) {
-        window.alert("shit is happening"); // TODO remove it
-      } finally {
-        _loading = false;
-        loaded.add(true);
       }
     }
   }
 
   void clearToken() {
     _loaded = null;
-    cookie.remove(tokenName);
+//    cookie.remove(tokenName);
   }
 }
